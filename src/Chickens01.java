1068a6abcd1cf4ff7f997ae41bca0bd06c2863ea
OwgerD <113381416+OwgerD@users.noreply.github.com>
2022-09-21 13:53:21 -0400

    Made changes
    Made changes

--- src/Chickens01.java
+++ src/Chickens01.java
@@ -2,6 +2,15 @@ public class Chickens01 {
     public static void main(String[] args) {
         //Put your code here
         
+        int eggsPerChicken = 4;
+        int chickenCount = 8;
+        int totalEggs = eggsPerChicken * chickenCount;
         System.out.println(totalEggs);
+        
+        int b,c;
+        b = chickenCount++;      
+        
+        
+        
     }   
 }
